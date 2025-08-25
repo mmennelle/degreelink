e552f852236fc89a2250e2de6e404c49570076fd
Mitchell Mennelle <mpmennel@uno.edu>
2025-08-25 16:58:31 -0500

    background gradients!

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,13 +7,19 @@ export default {
     darkMode: 'class', // Enable dark mode support
     theme: {
       extend: {
-        colors:{
-          surface:{
-            light: '#e6e7e6fb',
-            dark: '#0d1520ff',
-          },
+       // colors:{
+         // surface:{
+           // light: '#e6e7e6fb',
+            //dark: '#0d1520ff',
+          //},
+        //},
+        backgroundImage:{
+          "surface-light": 'linear-gradient(to right, var(--tw-gradient-from), var(--tw-gradient-via, transparent), var(--tw-gradient-to))',
+          "surface-dark": 'linear-gradient(to right, var(--tw-gradient-from), var(--tw-gradient-via, transparent), var(--tw-gradient-to))',
         },
       },
     },
     plugins: [],
   }
+
+  
