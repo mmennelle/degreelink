cd497cd70ceafd5c33f7c7ac5085893570e2793a
Mitchell Mennelle <mpmennel@uno.edu>
2026-02-10 09:50:45 -0600

    grfana items

---
+++ grafana/setup_readonly_user.sql
@@ -0,0 +1,17 @@
+-- Create read-only user for Grafana
+-- Run this in your Postgres database: psql -d course_transfer -f grafana/setup_readonly_user.sql
+
+CREATE USER grafana_readonly WITH PASSWORD 'GrafanaReadOnly2025!';
+
+-- Grant connect privilege
+GRANT CONNECT ON DATABASE course_transfer TO grafana_readonly;
+
+-- Grant usage on schema
+GRANT USAGE ON SCHEMA public TO grafana_readonly;
+
+-- Grant SELECT on all existing tables
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO grafana_readonly;
+
+-- Grant SELECT on future tables
+ALTER DEFAULT PRIVILEGES IN SCHEMA public 
+GRANT SELECT ON TABLES TO grafana_readonly;
