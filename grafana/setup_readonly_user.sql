23de1af936f172b10c889a1b7a9aa247bccf1127
Mitchell Mennelle <mpmennel@uno.edu>
2026-02-12 13:20:22 -0600

    grafana now has a .env

--- grafana/setup_readonly_user.sql
+++ grafana/setup_readonly_user.sql
@@ -1,7 +1,8 @@
 -- Create read-only user for Grafana
 -- Run this in your Postgres database: psql -d course_transfer -f grafana/setup_readonly_user.sql
+-- Replace YOUR_SECURE_PASSWORD with the same password from .env.grafana
 
-CREATE USER grafana_readonly WITH PASSWORD 'GrafanaReadOnly2025!';
+CREATE USER grafana_readonly WITH PASSWORD 'YOUR_SECURE_PASSWORD';
 
 -- Grant connect privilege
 GRANT CONNECT ON DATABASE course_transfer TO grafana_readonly;
